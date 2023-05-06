796577d0d948dfed0dfa41be9bb6560b03357ffe
Александр Кошелев <Koshelevsanya1994@yandex.ru>
2023-05-06 13:27:07 +0300

    Добавлено главное меню

--- bot/binance_bot.py
+++ bot/binance_bot.py
@@ -21,8 +21,20 @@ dp = Dispatcher(bot)
 async def start(message: types.Message):
     if not BotDB.user_exists(message.from_user.id):
         BotDB.add_user(message.from_user.id)
-
     await bot.send_message(chat_id=message.chat.id, text='Добро пожаловать')
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ['Узнать цену BTC', 'Выбрать валюту']
+    keyboard.add(*buttons)
+    await bot.send_message(chat_id=message.chat.id, text='Выберете действие', reply_markup=keyboard)
+
+
+@dp.message_handler(commands='main_menu')
+async def main_menu(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ['Узнать цену BTC', 'Выбрать валюту']
+    keyboard.add(*buttons)
+    await bot.send_message(chat_id=message.chat.id, text='Выберете действие', reply_markup=keyboard)
+
 
 
 if __name__ == '__main__':
